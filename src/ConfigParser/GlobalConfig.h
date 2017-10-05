4d1a232bcfb74c5a92618736d9bb119226b3fc76
losfair <zhy20000919@hotmail.com>
2017-10-05 18:05:49 +0800

    Default listen_addr to 127.0.0.1:8189

--- src/ConfigParser/GlobalConfig.h
+++ src/ConfigParser/GlobalConfig.h
@@ -5,6 +5,7 @@
 namespace deepdark {
 
 GlobalConfig::GlobalConfig() {
+    listen_addr = "127.0.0.1:8189";
 }
 
 std::unique_ptr<GlobalConfig> GlobalConfig::load(const std::string& config) {
@@ -12,7 +13,7 @@ std::unique_ptr<GlobalConfig> GlobalConfig::load(const std::string& config) {
     std::unique_ptr<GlobalConfig> ret(new GlobalConfig());
 
     ret -> service_config_directory = fields["service_config_directory"];
-    ret -> listen_addr = fields["listen_addr"];
+    if(fields.find("listen_addr") != fields.end()) ret -> listen_addr = fields["listen_addr"];
 
     return ret;
 }
