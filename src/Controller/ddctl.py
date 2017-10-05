17e5caca0f812cd4863813c0539797e59a959dbe
losfair <zhy20000919@hotmail.com>
2017-10-05 17:57:19 +0800

    ddctl

--- src/Controller/ddctl.py
+++ src/Controller/ddctl.py
@@ -1,18 +1,54 @@
 import pyice
 import asyncio
+import argparse
 
 async def run():
-    client = pyice.rpc.Client("127.0.0.1:8189")
+    parser = argparse.ArgumentParser(
+        description = "Command-line interface for DeepDark"
+    )
+    parser.add_argument(
+        "--remote",
+        dest = "remote_addr",
+        default = "127.0.0.1:8189",
+        help = "Remote address to connect"
+    )
+    parser.add_argument(
+        "action",
+        type = str
+    )
+    parser.add_argument(
+        "opt1",
+        type = str,
+        nargs = "?"
+    )
+    args = parser.parse_args()
+
+    client = pyice.rpc.Client(args.remote_addr)
     pool = pyice.rpc.ClientConnectionPool(client)
     await pool.init()
 
-    ret = await pool.call("get_status", [])
-    print(ret.get_string())
+    if args.action == "status":
+        ret = await pool.call("get_status", [])
+        print(ret.get_string())
+    elif args.action == "start" or args.action == "stop":
+        target = args.opt1
+        if target == None:
+            raise Exception("Service name required")
 
-    ret = await pool.call("stop_service", [
-        pyice.rpc.Param.build_string("test_service")
-    ]);
-    print(ret.get_bool())
+        if args.action == "start":
+            op_name = "start_service"
+        else:
+            op_name = "stop_service"
+
+        ret = await pool.call(op_name, [
+            pyice.rpc.Param.build_string(target)
+        ])
+        if ret.get_bool():
+            print("OK")
+        else:
+            print("Error")
+    else:
+        print("Unknown action: " + args.action)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(run())
