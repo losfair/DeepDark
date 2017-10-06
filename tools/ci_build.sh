a42eca150bfa95919e5d2e99a549d55190523d71
losfair <zhy20000919@hotmail.com>
2017-10-06 10:27:23 +0800

    Set CC & CXX

--- tools/ci_build.sh
+++ tools/ci_build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export CC=gcc-6
+export CXX=g++-6
 apt-get install -y build-essential autoconf libtool libgflags-dev libgtest-dev clang libc++-dev
 git clone https://github.com/grpc/grpc
 cd grpc
