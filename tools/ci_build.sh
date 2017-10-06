cd77f625df9a2a966563affcc5af57a5a4a2ae94
losfair <zhy20000919@hotmail.com>
2017-10-06 10:16:31 +0800

    Travis build configurations

---
+++ tools/ci_build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt-get install -y build-essential autoconf libtool libgflags-dev libgtest-dev clang libc++-dev
+git clone https://github.com/grpc/grpc
+cd grpc
+git submodule update --init
+make -j5
+make install
+cd third_party/protobuf
+make install
+cd ../..
+cd ..
+mkdir target
+make
